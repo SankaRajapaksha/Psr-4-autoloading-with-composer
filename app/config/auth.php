c16694e41f4a0408f50ebf11844ae7bfdf1aadc3
SANKA <sanka@3cs.solutions>
2018-10-19 13:28:23 +0530

    psuh files

---
+++ app/config/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+ 
+namespace App\Config;
+
+class Setings {
+
+    public function __construct()
+    {
+        echo "This is the constructor";
+    }
+
+    public function print()
+    {
+        echo "this is the function from class";
+    }
+}
+
+
+?>
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once 'app/autoload.php';
+
+use App\Config\Setings as test;
+ 
+//calling Class
+$obj = new test();
+
+echo '<br>';
+
+//Calling function
+$obj->print();
+
+ 
+
+
+?>
